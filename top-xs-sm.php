df43f5029c4b52b33bab25ddf537ddc1cc0b047a
lo1cgsan <lo1cgsan@gmail.com>
2022-04-10 20:01:00 +0200

    top-xs-sm links now read from xml

--- top-xs-sm.php
+++ top-xs-sm.php
@@ -19,9 +19,12 @@
   <div class="row d-md-none">
     <div class="col-xs-12">
       <div class="text-center mt-3">
-		<a class="btn-system btn-mini" href="http://lo1.sandomierz.pl/rekrutacja">Rekrutacja</a>
-		<a class="btn-system btn-mini" href="http://lo1.sandomierz.pl/plany/ostatni/index.html">Plan lekcji</a>
-        <a class="btn-system btn-mini" href="http://lo1.sandomierz.pl/e-dziennik">E-dziennik</a>
+<?php
+  $dane = get_dane('top-xs-sm.xml');
+  foreach ($dane as $i) {
+    echo('<a class="btn-system btn-mini" href="'.get_site_url(false).$i['link'].'">'.$i['title'].'</a>');
+  }
+?>
       </div>
     </div>
   </div>
